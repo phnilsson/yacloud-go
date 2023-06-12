c8953c2dfb340e5d2f3b33940ff6ef7c87233d86
Philip Nilsson <ph.nilsson@gmail.com>
2023-06-12 17:37:13 +0200

    Slight cleanup in main.go

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"net/url"
-	"os"
 
 	"time"
 
@@ -20,13 +18,6 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/cors"
 )
-
-type PageView struct {
-	CurrentUser string
-	PageTitle   string
-	Title       string
-	Text        string
-}
 
 type LoginView struct {
 	CurrentUser string
@@ -58,28 +49,6 @@ func CreateToken(userId int) (string, error) {
 	return token, nil
 }
 
-func start(c *gin.Context) {
-	fmt.Println("In Start")
-	session := sessions.Default(c)
-	user := session.Get(userkey)
-	var currentUser = ""
-	if user != nil {
-		currentUser = user.(string)
-	}
-	computerName, _ := os.Hostname()
-	c.HTML(http.StatusOK, "home.html", &PageView{CurrentUser: currentUser, PageTitle: "test", Title: "Hej Golang", Text: computerName})
-}
-
-func secretfunc(c *gin.Context) {
-	session := sessions.Default(c)
-	user := session.Get(userkey)
-	var currentUser = ""
-	if user != nil {
-		currentUser = user.(string)
-	}
-	c.HTML(http.StatusOK, "secret.html", &PageView{CurrentUser: currentUser, PageTitle: "test", Title: "Hej Golang", Text: "hejsan"})
-}
-
 func logout(c *gin.Context) {
 	session := sessions.Default(c)
 	session.Delete(userkey)
@@ -93,9 +62,6 @@ func new_user(c *gin.Context) {
 }
 
 func newUserPost(c *gin.Context) {
-
-	// username := c.PostForm("Email")
-	// password := c.PostForm("password")
 	var cred Credential
 	c.ShouldBindJSON(&cred)
 	passwordHash, err := bcrypt.GenerateFromPassword([]byte(cred.Password), bcrypt.DefaultCost)
@@ -118,16 +84,8 @@ func login(c *gin.Context) {
 func loginPost(c *gin.Context) {
 	fmt.Println("LoginPost")
 	var cred Credential
-	session := sessions.Default(c)
 
 	c.ShouldBindJSON(&cred)
-
-	fmt.Println("password:")
-	fmt.Println(cred.Password)
-	fmt.Println("email:")
-	fmt.Println(cred.Email)
-	fmt.Println(cred)
-
 	var user data.User
 	data.DB.Where("Email = ?", cred.Email).First(&user)
 
@@ -139,10 +97,6 @@ func loginPost(c *gin.Context) {
 	}
 
 	token, _ := CreateToken(user.Id)
-
-	session.Set("user_id", user.Id)
-	session.Save()
-	fmt.Println("Returning json at end of function")
 	c.JSON(http.StatusOK, gin.H{"token": token})
 }
 
@@ -166,41 +120,15 @@ func main() {
 	corsConfig.AllowHeaders = []string{"Origin", "Content-Type"}
 
 	router.Use(cors.New(corsConfig))
-	//2
 	var secret = []byte("secret")
-	// store, _ := redis.NewStore(10, "tcp", config.Redis.Server, "", secret)
 	store := memstore.NewStore([]byte(secret))
 	router.Use(sessions.Sessions("mysession2", store))
 
 	router.LoadHTMLGlob("templates/**")
-	router.GET("/", start)
 	router.GET("/login", login)
 	router.POST("/login", loginPost)
 	router.GET("/logout", logout)
 	router.GET("/new_user", new_user)
 	router.POST("/new_user", newUserPost)
-
-	//3 frivillig
-	adminRoutes := router.Group("/admin")
-	adminRoutes.Use(AuthRequired)
-	adminRoutes.GET("/account", secretfunc)
-
 	router.Run(":8080")
-}
-
-// 4 frivillig
-func AuthRequired(c *gin.Context) {
-	session := sessions.Default(c)
-	user := session.Get(userkey)
-	fmt.Println("user:")
-	fmt.Println(user)
-	var redirectUrl = url.QueryEscape("http://" + c.Request.Host + c.Request.RequestURI)
-	if user == nil {
-		c.Redirect(302, "/login?redirect_uri="+redirectUrl)
-		// Abort the request with the appropriate error code
-		return
-	}
-	fmt.Println("Authenticated")
-	// Continue down the chain to handler etc
-	c.Next()
 }
